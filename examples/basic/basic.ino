648fdba3a15b495490278a51eb7c7fbcfb6f6c17
Jeff Epler <jepler@gmail.com>
2020-10-07 14:29:43 -0500

    add basic sending example

---
+++ examples/basic/basic.ino
@@ -0,0 +1,28 @@
+#include <Ardiuino.h>
+
+#include <Adafruit_ZeroCAN.h>
+
+Adafruit_ZeroCAN can{};
+
+void setup() {
+    Serial.begin(9600); // open the serial port at 9600 bps:
+
+    digitalWrite(PIN_CAN_STANDBY, false); // turn off STANDBY
+    can.begin(1000000, true, false);
+}
+
+Adafruit_ZeroCAN::Message m{0xaa, false, false, 8, {'Z', 'e', 'r', 'o', 'C', 'A', 'N', 0}};
+
+void loop() {
+    auto state = can.busState();
+    Serial.print("bus state ");    
+    Serial.println((int)state);
+
+    if(state == Adafruit_ZeroCAN::BUS_OFF) {
+        can.restart();
+    }
+
+    m.data[7] ++;
+    can.send(m);
+    delay(250);
+}
