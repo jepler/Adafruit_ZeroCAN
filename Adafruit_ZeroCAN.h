99f5886fd151d9480320c1440202ad9f6ec7cf64
Jeff Epler <jepler@gmail.com>
2020-10-07 14:31:34 -0500

    run clang-format

--- Adafruit_ZeroCAN.h
+++ Adafruit_ZeroCAN.h
@@ -21,50 +21,47 @@ struct _adafruit_ZeroCAN_state;
 
 class Adafruit_ZeroCAN {
 public:
-    enum BusState {
-        ERROR_ACTIVE, ERROR_WARNING, ERROR_PASSIVE, BUS_OFF
-    };
+  enum BusState { ERROR_ACTIVE, ERROR_WARNING, ERROR_PASSIVE, BUS_OFF };
 
-    struct Message {
-        uint32_t id;
-        bool extended;
-        bool rtr;
-        uint8_t size;
-        uint8_t data[8];
-    };
+  struct Message {
+    uint32_t id;
+    bool extended;
+    bool rtr;
+    uint8_t size;
+    uint8_t data[8];
+  };
 
-    struct Match {
-        uint32_t id;
-        uint32_t mask;
-        bool extended;
-    };
+  struct Match {
+    uint32_t id;
+    uint32_t mask;
+    bool extended;
+  };
 
-    class Listener {
-    public:
-        Listener();
-        bool begin(Adafruit_ZeroCAN &can, Match *matches, size_t nmatch);
-        ~Listener();
-        bool in_waiting() const;
-        bool read(Message &);
-    };
+  class Listener {
+  public:
+    Listener();
+    bool begin(Adafruit_ZeroCAN &can, Match *matches, size_t nmatch);
+    ~Listener();
+    bool in_waiting() const;
+    bool read(Message &);
+  };
 
-    Adafruit_ZeroCAN(uint8_t TX_PIN, uint8_t RX_PIN);
-    Adafruit_ZeroCAN();
-    ~Adafruit_ZeroCAN() {}
-    
-    bool begin(int baudrate, bool loopback, bool silent);
+  Adafruit_ZeroCAN(uint8_t TX_PIN, uint8_t RX_PIN);
+  Adafruit_ZeroCAN();
+  ~Adafruit_ZeroCAN() {}
 
-    int transmitErrorCount() const;
-    int receiveErrorCount() const;
-    BusState busState() const;
+  bool begin(int baudrate, bool loopback, bool silent);
 
-    bool send(const Message &m);
-    void restart();
+  int transmitErrorCount() const;
+  int receiveErrorCount() const;
+  BusState busState() const;
+
+  bool send(const Message &m);
+  void restart();
 
 private:
-    struct State;
-    int8_t _tx, _rx;
-    void *_hw;
-    _adafruit_ZeroCAN_state *state;
+  struct State;
+  int8_t _tx, _rx;
+  void *_hw;
+  _adafruit_ZeroCAN_state *state;
 };
-
